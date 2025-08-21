a335d66d2f4770b9d268169932dd53ee721003af
Moises Tabar <moisestabar@Moisess-MacBook-Pro.local>
2025-08-20 21:15:36 -0400

    chore: Adding pre commit configuration for linting

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from dotenv import load_dotenv
-from prompts import SYSTEM_PROMPT, USER_PROMPT
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import chat_agent_executor
+
+from prompts import SYSTEM_PROMPT, USER_PROMPT
 from tools import git_diff
-import os
 
 load_dotenv()
 
@@ -14,17 +16,10 @@ model = ChatOpenAI(
     temperature=0,
 )
 
-agent = chat_agent_executor.create_tool_calling_executor(
-    model=model,
-    tools=[git_diff]
-)
+agent = chat_agent_executor.create_tool_calling_executor(model=model, tools=[git_diff])
 
-result = agent.invoke({
-    "messages": [
-        ("system", SYSTEM_PROMPT),
-        ("user", USER_PROMPT)
-    ]
-})
+result = agent.invoke({"messages": [("system", SYSTEM_PROMPT), ("user", USER_PROMPT)]})
+
 
 if __name__ == "__main__":
     print(result["messages"][-1].content.strip())
--- tools.py
+++ tools.py
@@ -1,5 +1,5 @@
-import subprocess
 import shlex
+import subprocess
 
 from langchain_core.tools import tool
 
@@ -7,10 +7,6 @@ from langchain_core.tools import tool
 @tool(description="Get the diff of the staged changes of the current branch.")
 def git_diff() -> str:
     try:
-        return subprocess.check_output(
-            shlex.split(
-                "git diff --staged"
-            )
-        ).decode('utf-8')
+        return subprocess.check_output(shlex.split("git diff --staged")).decode("utf-8")
     except subprocess.CalledProcessError as e:
         return f"Error running git diff: {e}"
