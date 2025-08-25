c22656395122905676c0a204c24fb5f61cc422e2
Moises Tabar <moisestabar@Moisess-MacBook-Pro.local>
2025-08-25 13:48:46 -0400

    feat: add Typer integration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import typer
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import chat_agent_executor
@@ -11,15 +12,29 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-model = ChatOpenAI(
-    model_name="gpt-4o-mini",
-    temperature=0,
-)
 
-agent = chat_agent_executor.create_tool_calling_executor(model=model, tools=[git_diff])
+def main() -> None:
+    """
+    Generate a commit message for the staged changes. \n
+    Must have staged changes to generate a commit message. \n
+    If no staged changes are found, the agent will return an error message.
+    """
 
-result = agent.invoke({"messages": [("system", SYSTEM_PROMPT), ("user", USER_PROMPT)]})
+    model = ChatOpenAI(
+        model_name="gpt-4o-mini",
+        temperature=0,
+    )
+
+    agent = chat_agent_executor.create_tool_calling_executor(
+        model=model, tools=[git_diff]
+    )
+
+    result = agent.invoke(
+        {"messages": [("system", SYSTEM_PROMPT), ("user", USER_PROMPT)]}
+    )
+
+    typer.secho(result["messages"][-1].content.strip(), fg=typer.colors.GREEN)
 
 
 if __name__ == "__main__":
-    print(result["messages"][-1].content.strip())
+    typer.run(main)
